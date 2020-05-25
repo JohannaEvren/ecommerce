8af15f2987127e9c1ebf4985061018384f5e224d
johannaEvren <johanna.evren@hotmail.com>
2020-05-25 13:12:08 +0200

    first commit

---
+++ public/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Shop</title>
+</head>
+<body>
+	<h1>Shop page</h1>
+
+	<a href="admin/adminProduct.php">ADMIN PRODUCTS</a>
+	<a href="admin/adminUsers.php">ADMIN USERS</a>
+
+</body>
+</html>
