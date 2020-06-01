03b119ed7f0283657c1c02283005dbd90ae83d74
IB-Del <60423401+IB-Del@users.noreply.github.com>
2020-06-01 12:05:15 +0200

    Add files via upload

--- public/index.php
+++ public/index.php
@@ -1,13 +1,49 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Shop</title>
-</head>
-<body>
-	<h1>Shop page</h1>
+<?php
+    
+      require('../src/dbconnect.php');
 
-	<a href="admin/adminProduct.php">ADMIN PRODUCTS</a>
-	<a href="admin/adminUsers.php">ADMIN USERS</a>
+      try{
+        $stmt  = $dbconnect->query("SELECT * FROM products");
+            $products = $stmt->fetchALL();
+         } catch (\PDOexception $e) {
 
-</body>
-</html>
+           throw new \PDOexception($e->getMessage(), $e->getCode());
+         };
+
+     include "layout/header.php";
+
+
+    $title    = "";
+    $description  = "";
+    $price   = "";
+
+?>
+
+<?php foreach ($products as $key => $text) { ?>
+<div class="row" name="showcard" value="<?=$text['id']?>">
+  <div class="col-sm-2">
+    <div class="card">
+      <div class="card-body" style="width: 12rem;">
+      <img class="card-img-top" src="." alt="Card image cap">
+        <h5 class="card-title"><?=htmlentities($text['title'])?></h5>
+        <p class="card-text"><?=htmlentities($text['description'])?></p>
+        <div class="btn btn-primary"><?=htmlentities($text['price'])?></div>
+       
+       
+       <!--
+       <a href='productpage.php?id=hey' class="btn btn-primary">Read More</a>
+       --->
+        
+        <form action='productpage.php' method="GET">
+            <input type="hidden" name="id" value="<?=$text['id']?>">
+            <input type="submit" value="read more">
+        </form>
+      </div>
+    </div>
+  </div>
+
+
+<?php } ?>
+
+
+
