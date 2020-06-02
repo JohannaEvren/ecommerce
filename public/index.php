a312428142884ddd919771aa4b85f455bcc873df
Ibrahimbajwa1 <Ibrahim.bajwa@hyperisland.se>
2020-06-02 11:05:40 +0200

    Cart

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@
      include "layout/header.php";
 
 
+
     $title    = "";
     $description  = "";
     $price   = "";
@@ -21,29 +22,31 @@
 
 <?php foreach ($products as $key => $text) { ?>
 <div class="row" name="showcard" value="<?=$text['id']?>">
-  <div class="col-sm-2">
+  <div class="col-sm-4 border p-4 d-flex flex-column" >
     <div class="card">
-      <div class="card-body" style="width: 12rem;">
+      <div class="card-body" style="margin-top:auto">
       <img class="card-img-top" src="." alt="Card image cap">
         <h5 class="card-title"><?=htmlentities($text['title'])?></h5>
         <p class="card-text"><?=htmlentities($text['description'])?></p>
         <div class="btn btn-primary"><?=htmlentities($text['price'])?></div>
-       
-       
-       <!--
-       <a href='productpage.php?id=hey' class="btn btn-primary">Read More</a>
-       --->
-        
+      
         <form action='productpage.php' method="GET">
             <input type="hidden" name="id" value="<?=$text['id']?>">
             <input type="submit" value="read more">
         </form>
+
+<form action="add-cart-item.php" method="POST">
+<input type="hidden" name="productId" value = "<?=$text['id']?>">
+<input type="number" name="quantity" value ="1" min= "0">
+<input type="submit" name="addToCart" value = "Add To Cart">
+
+</form>
+
       </div>
     </div>
   </div>
 
 
 <?php } ?>
-
 
 
--- public/productpage.php
+++ public/productpage.php
@@ -22,25 +22,27 @@
     $title            = "";
     $description     = "";
     $price           = "";
-   
- echo "<pre>";
-print_r($_GET);
-echo "</pre>";
+
+
 ?>
 
 
 <div class="row" name="showcard" value="<?=$products['id']?>">
-  <div class="col-sm-6">
+  <div class="col-sm-4" style="width: 6rem; height: 6rem;">
     <div class="card">
-      <div class="card-body" style="width: 16rem;">
+      <div class="card-body">
       <img class="card-img-top" src="..." alt="Card image cap">
         <h5 class="card-title"><?=htmlentities($products['title'])?></h5>
         <p class="card-text"><?=htmlentities($products['description'])?></p>
         <span><?=htmlentities($products['price'])?></span>
-        <a href="#" class="cart-btn">Add to cart</a>
+        <form action="add-cart-item.php" method="POST">
+<input type="hidden" name="productId" value = "<?=$text['id']?>">
+<input type="number" name="quantity" value ="1" min= "0">
+<input type="submit" name="addToCart" value = "Add To Cart">
+
+</form>
       </div>
       </div>
     </div>
   </div>
-
 
