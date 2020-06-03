d538b6b0b5452dd2ac8dad51954eb8cc1611c6c1
IB-Del <60423401+IB-Del@users.noreply.github.com>
2020-06-03 13:43:07 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,5 +1,5 @@
 <?php
-    
+      require('../src/config.php');
       require('../src/dbconnect.php');
 
       try{
@@ -9,8 +9,7 @@
 
            throw new \PDOexception($e->getMessage(), $e->getCode());
          };
-
-     include "layout/header.php";
+    include "layout/header.php";   
 
 
 
@@ -19,34 +18,37 @@
     $price   = "";
 
 ?>
+<div class="container">
+  <?php include "cart.php";?>
+  <?php foreach ($products as $key => $text) { ?>
+  <div class="row" name="showcard" value="<?=$text['id']?>">
+    <div class="col-sm-4 border p-4 d-flex flex-column" >
+      <div class="card">
+        <div class="card-body" style="margin-top:auto">
+        <img class="card-img-top" src="." alt="Card image cap">
+          <h5 class="card-title"><?=htmlentities($text['title'])?></h5>
+          <p class="card-text"><?=htmlentities($text['description'])?></p>
+          <div class="btn btn-primary"><?=htmlentities($text['price'])?></div>
+        
+          <form action='productpage.php' method="GET">
+              <input type="hidden" name="id" value="<?=$text['id']?>">
+              <input type="submit" value="read more">
+          </form>
 
-<?php foreach ($products as $key => $text) { ?>
-<div class="row" name="showcard" value="<?=$text['id']?>">
-  <div class="col-sm-4 border p-4 d-flex flex-column" >
-    <div class="card">
-      <div class="card-body" style="margin-top:auto">
-      <img class="card-img-top" src="." alt="Card image cap">
-        <h5 class="card-title"><?=htmlentities($text['title'])?></h5>
-        <p class="card-text"><?=htmlentities($text['description'])?></p>
-        <div class="btn btn-primary"><?=htmlentities($text['price'])?></div>
-      
-        <form action='productpage.php' method="GET">
-            <input type="hidden" name="id" value="<?=$text['id']?>">
-            <input type="submit" value="read more">
-        </form>
+  <form action="add-cart-item.php" method="POST">
+  <input type="hidden" name="productId" value = "<?=$text['id']?>">
+  <input type="number" name="quantity" value ="1" min= "0">
+  <input type="submit" name="addToCart" value = "Add To Cart">
 
-<form action="add-cart-item.php" method="POST">
-<input type="hidden" name="productId" value = "<?=$text['id']?>">
-<input type="number" name="quantity" value ="1" min= "0">
-<input type="submit" name="addToCart" value = "Add To Cart">
+  </form>
 
-</form>
-
+        </div>
       </div>
     </div>
   </div>
 
 
-<?php } ?>
+  <?php } ?>
+</div>
 
 
