96a33f4644445717e0fcf561cefe7bebba81ed8e
IB-Del <60423401+IB-Del@users.noreply.github.com>
2020-06-09 09:37:34 +0200

    Update and rename index.php to products.php

--- public/products.php
+++ public/products.php
@@ -18,37 +18,39 @@
     $price   = "";
 
 ?>
+<?php include "cart.php";?>
 <div class="container">
-  <?php include "cart.php";?>
-  <?php foreach ($products as $key => $text) { ?>
-  <div class="row" name="showcard" value="<?=$text['id']?>">
-    <div class="col-sm-4 border p-4 d-flex flex-column" >
+  <?php foreach ($products as $key => $products) { ?>
+  <div class="row" name="showcard" value="<?=$products['id']?>">
+  <div class="col-md-4" style="padding:15px;">
+  <div style="display:inline-block; border:solid 1px #808080; padding:15px">
       <div class="card">
-        <div class="card-body" style="margin-top:auto">
+        <div class="card-body">
         <img class="card-img-top" src="." alt="Card image cap">
-          <h5 class="card-title"><?=htmlentities($text['title'])?></h5>
-          <p class="card-text"><?=htmlentities($text['description'])?></p>
-          <div class="btn btn-primary"><?=htmlentities($text['price'])?></div>
+          <h5 class="card-title"><?=htmlentities($products['title'])?></h5>
+          <p class="card-text"><?=htmlentities($products['description'])?></p>
+          <div class="price-info"><?=htmlentities($products['price'])?></div>
         
           <form action='productpage.php' method="GET">
-              <input type="hidden" name="id" value="<?=$text['id']?>">
+              <input type="hidden" name="id" value="<?=$products['id']?>">
               <input type="submit" value="read more">
           </form>
 
   <form action="add-cart-item.php" method="POST">
-  <input type="hidden" name="productId" value = "<?=$text['id']?>">
+  <input type="hidden" name="productId" value = "<?=$products['id']?>">
   <input type="number" name="quantity" value ="1" min= "0">
   <input type="submit" name="addToCart" value = "Add To Cart">
 
   </form>
-
+</div>
         </div>
       </div>
     </div>
   </div>
 
 
+
+
   <?php } ?>
 </div>
-
 
