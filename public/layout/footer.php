d5c0bd338bd41a8b3b75db99dfefb3ac30833ad8
IB-Del <60423401+IB-Del@users.noreply.github.com>
2020-06-15 13:11:19 +0200

    Create footer.php

---
+++ public/layout/footer.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+  <link rel="stylesheet" type="text/css" href="css/stylefooter.css">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1, user-scalable=no">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+<body>
+<footer>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-12 col-md">Inredning <small class="d-block my-3 text-muted">© 2020</small> 
+    <a href="mailto:hello@.com" title="mail us"><p>Mail: hello@.com</p></a></div>
+      <div class="col-6 col-md">
+        <h5><b>Features</b></h5>
+        <ul class="list-unstyled text-small">
+          <li>
+            <a class="text-muted" href="#">Cool stuff</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Random feature</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Team feature</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Stuff for developers</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Another one</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Last time</a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-6 col-md">
+        <h5><b>About</b></h5>
+        <ul class="list-unstyled text-small">
+          <li>
+            <a class="text-muted" href="#">Team</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Locations</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Privacy</a>
+          </li>
+          <li>
+            <a class="text-muted" href="#">Terms</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</footer>
+</body>
+</html>
