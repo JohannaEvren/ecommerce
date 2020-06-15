9ca29925e2b438f71248a70181f094180127fe50
johannaEvren <johanna.evren@hotmail.com>
2020-06-15 11:12:23 +0200

    checkout style+ confirmationPage

--- public/order-confirmation.php
+++ public/order-confirmation.php
@@ -1,1 +1,129 @@
-<h1>RÄTT</h1>
+<?php
+       require('../src/config.php');
+       require('../src/dbconnect.php');
+
+               $cartItemCount = count($_SESSION['cartItems']);
+               $cartTotalSum = 0;
+                foreach ($_SESSION['cartItems'] as $cartId => $cartItem) {
+                $cartTotalSum += $cartItem['price'] * $cartItem['quantity'];
+              }; 
+
+
+
+
+      try {
+       
+        $query = "
+        SELECT * FROM orders
+        WHERE id = :id";
+
+        $stmt = $dbconnect->prepare($query);
+        $stmt->bindvalue(':id', $_SESSION['orderIdConfirmation']);
+        $stmt->execute();
+        
+        $order = $stmt->fetch();
+    } catch (\PDOException $e) {
+      throw new \PDOException($e->getMessage(), (int) $e->getCode());
+    }
+
+  
+      $users = fetchUsersById($order['user_id']);
+
+
+
+
+
+
+
+
+
+
+      
+
+      
+       
+      ?>
+<!DOCTYPE html>
+<html lang="sv"> 
+<head>
+  <meta charset="utf-8">
+  
+  <title><?php $PageTitle ?></title>
+
+  <link rel="stylesheet" type="text/css" href="css/style_main.css"> 
+<meta name="viewport" content="width=device-width, initial-scale=1.0 maximum-scale=1, user-scalable=no">
+
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+
+</head>
+  
+   
+
+  <div class="container-fluid">
+
+    <div class="row">
+
+
+
+
+          <div class="offset-1 col-10 showCart">
+            <div class="shoppingCart">
+             <h1 id="title">Thank you for your order <?=$users['first_name']?></h1><br><br>
+             <h4>Your order confirmation ID is <?=$order['id']?> </h4>
+           </div>
+            <div class="offset col-10"> 
+            
+
+          <?php foreach($_SESSION['cartItems'] as $cartId => $cartItem){ ?>
+
+                    <div class="offset-1 col-12"> 
+                      <div class="row">
+                          <div class="col-2"><?=$cartItem['title']?></div> 
+                          <div class="col-8"><?=$cartItem['description']?></div>
+                          <div class="col-2"><h5><?=$cartItem['price']?> SEK</h5></div>
+                        </tr>
+                        <hr>
+                     
+                        </div>
+                      </div>
+
+
+          
+          
+
+               <?php };?>
+
+
+
+            </div>
+
+               <div class="offset-8 col-3">
+                <h3>Total: <?=$cartTotalSum?> $ </h3>
+              </div>
+              <br><br><br><br><br>
+           
+                <h4 style="text-align: center;"> <a style="color:red;" href="products.php">BACK TO SHOP</a></h4>
+                 </div>
+
+
+
+                 <?php 
+
+                 unset($_SESSION['cartItems']); 
+                unset($_SESSION['orderIdConfirmation']);   
+
+
+                 ?>
+
+
+
+
+
+
+
+
+
+
+        
+
+
--- public/productpage.php
+++ public/productpage.php
@@ -43,7 +43,7 @@
         <span><?=htmlentities($products['price'])?></span>
         
         <form action="add-cart-item.php" method="POST">
-<input type="hidden" name="productId" value = "<?=$_POST['productId']?>">
+<input type="hidden" name="productId" value="<?=$products['id']?>">
 <input type="number" name="quantity" value ="1" min= "0">
 <input type="submit" name="addToCart" value = "Add To Cart">
 </form>
@@ -51,5 +51,11 @@
       </div>
     </div>
   </div>
+   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+            <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
 
 
